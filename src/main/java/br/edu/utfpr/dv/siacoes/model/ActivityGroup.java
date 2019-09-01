5149cfe3cc9e20a23793947331499436357e6627
marquesr1 <rafael.fmarques@outlook.com>
2019-09-01 16:36:53 -0300

    Lombok aplicada nas classes Activity e ActivityGroup

--- src/main/java/br/edu/utfpr/dv/siacoes/model/ActivityGroup.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/model/ActivityGroup.java
@@ -1,13 +1,20 @@
 ﻿package br.edu.utfpr.dv.siacoes.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
 import java.io.Serializable;
 
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(onlyExplicitlyIncluded = true)
 public class ActivityGroup implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
-	
-	private int idActivityGroup;
-	private String description;
+
+	@EqualsAndHashCode.Include private int idActivityGroup;
+	@ToString.Include private String description;
 	private int sequence;
 	private int minimumScore;
 	private int maximumScore;
@@ -19,56 +26,5 @@ public class ActivityGroup implements Serializable {
 		this.setMinimumScore(0);
 		this.setMaximumScore(0);
 	}
-	
-	public int getIdActivityGroup() {
-		return idActivityGroup;
-	}
-	public void setIdActivityGroup(int idActivityGroup) {
-		this.idActivityGroup = idActivityGroup;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public int getSequence() {
-		return sequence;
-	}
-	public void setSequence(int sequence) {
-		this.sequence = sequence;
-	}
-	public int getMinimumScore() {
-		return minimumScore;
-	}
-	public void setMinimumScore(int minimumScore) {
-		this.minimumScore = minimumScore;
-	}
-	public int getMaximumScore() {
-		return maximumScore;
-	}
-	public void setMaximumScore(int maximumScore) {
-		this.maximumScore = maximumScore;
-	}
-	
-	public String toString(){
-		return this.getDescription();
-	}
-	
-	@Override
-    public int hashCode() {
-        return this.getIdActivityGroup();
-    }
-	
-	@Override
-    public boolean equals(final Object object) {
-        if (!(object instanceof ActivityGroup)) {
-            return false;
-        }else if(this.getIdActivityGroup() == ((ActivityGroup)object).getIdActivityGroup()){
-        	return true;
-        }else{
-        	return false;
-        }
-    }
 
 }
