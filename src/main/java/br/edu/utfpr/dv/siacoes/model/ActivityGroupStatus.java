91a96a89da7e200e8eff115c127dd74f3702e10b
marquesr1 <rafael.fmarques@outlook.com>
2019-09-01 16:41:19 -0300

    Lombok aplicada nas classes ActivityGroupStatus e ActivityScore

--- src/main/java/br/edu/utfpr/dv/siacoes/model/ActivityGroupStatus.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/model/ActivityGroupStatus.java
@@ -1,7 +1,12 @@
 package br.edu.utfpr.dv.siacoes.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 
+@Getter
+@Setter
 public class ActivityGroupStatus implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -12,19 +17,6 @@ public class ActivityGroupStatus implements Serializable {
 	public ActivityGroupStatus() {
 		this.setGroup(new ActivityGroup());
 		this.setAverageScore(0);
-	}
-	
-	public ActivityGroup getGroup() {
-		return group;
-	}
-	public void setGroup(ActivityGroup group) {
-		this.group = group;
-	}
-	public double getAverageScore() {
-		return averageScore;
-	}
-	public void setAverageScore(double averageScore) {
-		this.averageScore = averageScore;
 	}
 
 }
--- src/main/java/br/edu/utfpr/dv/siacoes/model/ActivityScore.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/model/ActivityScore.java
@@ -1,7 +1,12 @@
 package br.edu.utfpr.dv.siacoes.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 
+@Getter
+@Setter
 public class ActivityScore implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -14,25 +19,6 @@ public class ActivityScore implements Serializable {
 		this.setIdActivity(0);
 		this.setActivity("");
 		this.setScore(0);
-	}
-	
-	public int getIdActivity() {
-		return idActivity;
-	}
-	public void setIdActivity(int idActivity) {
-		this.idActivity = idActivity;
-	}
-	public String getActivity() {
-		return activity;
-	}
-	public void setActivity(String activity) {
-		this.activity = activity;
-	}
-	public double getScore() {
-		return score;
-	}
-	public void setScore(double score) {
-		this.score = score;
 	}
 
 }
