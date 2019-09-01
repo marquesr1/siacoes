a697546a1abf20b2547437c28d5f74030fdd6378
marquesr1 <rafael.fmarques@outlook.com>
2019-09-01 16:45:09 -0300

    Lombok aplicada nas classes ActivitySubmissionDetailReport e ActivitySubmissionFooterReport

--- src/main/java/br/edu/utfpr/dv/siacoes/model/ActivitySubmissionDetailReport.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/model/ActivitySubmissionDetailReport.java
@@ -3,7 +3,11 @@
 import java.io.Serializable;
 
 import br.edu.utfpr.dv.siacoes.util.DateUtils;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class ActivitySubmissionDetailReport implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -26,55 +30,6 @@ public class ActivitySubmissionDetailReport implements Serializable {
 		this.setTotal(0);
 		this.setSemester(DateUtils.getSemester());
 		this.setYear(DateUtils.getYear());
-	}
-	
-	public String getActivity() {
-		return activity;
-	}
-	public void setActivity(String activity) {
-		this.activity = activity;
-	}
-	public int getGroup() {
-		return group;
-	}
-	public void setGroup(int group) {
-		this.group = group;
-	}
-	public String getUnit() {
-		return unit;
-	}
-	public void setUnit(String unit) {
-		this.unit = unit;
-	}
-	public double getScore() {
-		return score;
-	}
-	public void setScore(double score) {
-		this.score = score;
-	}
-	public double getAmount() {
-		return amount;
-	}
-	public void setAmount(double amount) {
-		this.amount = amount;
-	}
-	public double getTotal() {
-		return total;
-	}
-	public void setTotal(double total) {
-		this.total = total;
-	}
-	public int getSemester() {
-		return semester;
-	}
-	public void setSemester(int semester) {
-		this.semester = semester;
-	}
-	public int getYear() {
-		return year;
-	}
-	public void setYear(int year) {
-		this.year = year;
 	}
 
 }
--- src/main/java/br/edu/utfpr/dv/siacoes/model/ActivitySubmissionFooterReport.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/model/ActivitySubmissionFooterReport.java
@@ -1,7 +1,12 @@
 ﻿package br.edu.utfpr.dv.siacoes.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 
+@Getter
+@Setter
 public class ActivitySubmissionFooterReport implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -22,49 +27,6 @@ public class ActivitySubmissionFooterReport implements Serializable {
 		this.setMaximum(0);
 		this.setSituation("");
 		this.setSequence(0);
-	}
-	
-	public int getIdActivityGroup() {
-		return idActivityGroup;
-	}
-	public void setIdActivityGroup(int idActivityGroup) {
-		this.idActivityGroup = idActivityGroup;
-	}
-	public String getGroup() {
-		return group;
-	}
-	public void setGroup(String group) {
-		this.group = group;
-	}
-	public double getTotal() {
-		return total;
-	}
-	public void setTotal(double total) {
-		this.total = total;
-	}
-	public int getMinimum() {
-		return minimum;
-	}
-	public void setMinimum(int minimum) {
-		this.minimum = minimum;
-	}
-	public int getMaximum() {
-		return maximum;
-	}
-	public void setMaximum(int maximum) {
-		this.maximum = maximum;
-	}
-	public String getSituation() {
-		return situation;
-	}
-	public void setSituation(String situation) {
-		this.situation = situation;
-	}
-	public int getSequence() {
-		return sequence;
-	}
-	public void setSequence(int sequence) {
-		this.sequence = sequence;
 	}
 
 }
