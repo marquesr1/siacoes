20b218b7da51c286db8cba8265b5b1c96e0b4ae5
marquesr1 <rafael.fmarques@outlook.com>
2019-09-16 22:31:23 -0300

    Código comum para todas subclasses

--- src/main/java/br/edu/utfpr/dv/siacoes/dao/ConfigDAO.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/dao/ConfigDAO.java
@@ -1,19 +1,69 @@
 package br.edu.utfpr.dv.siacoes.dao;
 
+import br.edu.utfpr.dv.siacoes.log.UpdateEvent;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public abstract class ConfigDAO<T> {
     public final T findByDepartment(int idDepartment) throws SQLException {
-        return null;
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try{
+            conn = ConnectionDAO.getInstance().getConnection();
+            stmt = conn.prepareStatement();
+
+            stmt.setInt(1, idDepartment);
+
+            rs = stmt.executeQuery();
+
+            if(rs.next()){
+                return this.loadObject(rs);
+            }else{
+                return null;
+            }
+        }finally{
+            if((rs != null) && !rs.isClosed())
+                rs.close();
+            if((stmt != null) && !stmt.isClosed())
+                stmt.close();
+            if((conn != null) && !conn.isClosed())
+                conn.close();
+        }
     }
 
     public final int save(int idUser, T config) throws SQLException{
-        return 1;
+        boolean insert = (this.findByDepartment(config.getDepartment().getIdDepartment()) == null);
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try{
+            conn = ConnectionDAO.getInstance().getConnection();
+
+            if(insert){
+                stmt = conn.prepareStatement();
+            }else{
+                stmt = conn.prepareStatement();
+            }
+
+            stmt.execute();
+
+            new UpdateEvent(conn).registerUpdate(idUser, config);
+
+            return config.getDepartment().getIdDepartment();
+        }finally{
+            if((stmt != null) && !stmt.isClosed())
+                stmt.close();
+            if((conn != null) && !conn.isClosed())
+                conn.close();
+        }
     }
 
     protected final T loadObject(ResultSet rs) throws SQLException{
-        return null;
-    }
 
+    }
 }
