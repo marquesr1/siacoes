cea7f143d20ae2c5b63500c7f66fb7b79f947367
marquesr1 <rafael.fmarques@outlook.com>
2019-09-16 22:07:01 -0300

    Metodos comuns para todas as subclasses

--- src/main/java/br/edu/utfpr/dv/siacoes/dao/ConfigDAO.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/dao/ConfigDAO.java
@@ -1,5 +1,19 @@
 package br.edu.utfpr.dv.siacoes.dao;
 
-public abstract class ConfigDAO {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public abstract class ConfigDAO<T> {
+    public final T findByDepartment(int idDepartment) throws SQLException {
+        return null;
+    }
+
+    public final int save(int idUser, T config) throws SQLException{
+        return 1;
+    }
+
+    protected final T loadObject(ResultSet rs) throws SQLException{
+        return null;
+    }
 
 }
